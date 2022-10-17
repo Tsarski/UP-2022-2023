e71232540a12c16156c0dda7155dc2f490e17325
YavorTsarski <32989540+Tsarski@users.noreply.github.com>
2022-10-17 19:06:56 +0300

    Create Task3-B.cpp

---
+++ week02/Task3-B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter four-digit number: ";
+    cin >> number;
+    int lastD = number % 10;
+    int secondD = number / 10 % 10;
+    int thirdD = number / 100 % 10;
+    int fourthD = number / 1000;
+
+    bool hasOddNumber = (lastD % 2 == 1) || (secondD % 2 == 1) || (thirdD % 2 == 1) || (fourthD % 2 == 1);
+
+    if (hasOddNumber) {
+        cout << "Has an odd number";
+    }
+    else {
+        cout << "Doesn't have an odd number";
+    }
+}
